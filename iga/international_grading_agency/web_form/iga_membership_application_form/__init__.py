71874a66b7a751e3227a9c83eb46bd9b2c0b181b
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-02-01 14:08:35 +0200

    creating the new implmentation plan

--- iga/international_grading_agency/web_form/iga_membership_application_form/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) 2026, Mustafa Nazier and contributors
-# For license information, please see license.txt
--- iga/international_grading_agency/web_form/iga_membership_application_form/iga_membership_application_form.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, Mustafa Nazier and contributors
-# For license information, please see license.txt
-
-import frappe
-
-def get_context(context):
-	"""Add custom context for web form"""
-	context.no_cache = 1

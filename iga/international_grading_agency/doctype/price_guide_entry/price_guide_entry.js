a089634237e5526f13066c5d1c7105a78754f778
mostafamohamed1984 <mustafanazieer@gmail.com>
2026-05-23 08:25:31 +0300

    adding the doctypes to the workspace

---
+++ iga/international_grading_agency/doctype/price_guide_entry/price_guide_entry.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Mustafa Nazier and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Price Guide Entry', {
+	// refresh: function(frm) {
+	// }
+});
---
+++ iga/international_grading_agency/doctype/submission_item_mint_error/submission_item_mint_error.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Mustafa Nazier and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Submission Item Mint Error', {
+	// refresh: function(frm) {
+	// }
+});
